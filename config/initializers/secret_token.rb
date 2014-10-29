5c81fd433cffb487d2b92264a1ab595a7f583584
vagrant <vagrant@rails-dev-box>
2014-10-29 18:18:13 +0000

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Upveil::Application.config.secret_key_base = '78565ecfcee881e5b03de8a92c914713008297cd63a4046ec66c6344baa92d0603da32df0b97c45ee887fa3f40f4c2914e1f4d4efc40483047eb2913eecc3b60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Upveil::Application.config.session_store :cookie_store, key: '_upveil_session'

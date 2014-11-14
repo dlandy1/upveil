eb9756f5b4ad411ec1d7806b46e578d51491edda
vagrant <vagrant@rails-dev-box>
2014-11-14 01:16:36 +0000

    fb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Upveil::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
-  config.app_domain = 'https://upveil.herokuapp.com/'
+  config.app_domain = 'upveil.herokuapp.com/'
 
   # Email
   config.action_mailer.delivery_method = :smtp
@@ -87,7 +87,7 @@ Upveil::Application.configure do
     user_name: 'someuser',
     password: 'somepass',
     authentication: :plain,
-    domain: 'https://upveil.herokuapp.com/'
+    domain: 'upveil.herokuapp.com/'
   }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.

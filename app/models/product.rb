5f7db0ada2ab56002c13be3acbacf515b592e14b
vagrant <vagrant@rails-dev-box>
2014-11-18 15:54:02 +0000

    arg

--- app/models/product.rb
+++ app/models/product.rb
@@ -125,7 +125,7 @@ class Product < ActiveRecord::Base
       0
     end
 
-     seconds = ((self.created_at.to_i - 1134028003)/8).to_f
+     seconds = ((self.created_at.to_i - 1134028003)/12).to_f
  
      new_rank = (order + sign.to_f * seconds / 45000).round(7)
  

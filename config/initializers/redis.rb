cb65767a45f03f00b93d4685a94c487c4c3a7075
vagrant <vagrant@rails-dev-box>
2014-11-18 14:31:51 +0000

    a

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,4 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-REDIS = Redis.new
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 HIGHSCORE_LB = Leaderboard.new('highscores', Leaderboard::DEFAULT_OPTIONS, {:redis_connection => REDIS})

61e3a9800a39367020ca3e67395156af050730e0
vagrant <vagrant@rails-dev-box>
2014-12-02 06:27:24 +0000

    ayoo

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,15 +3,18 @@ require 'rubygems'
 require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = "http://www.upveil.com"
 
-SitemapGenerator::Sitemap.create do
-  Category.find_each do |category|
-    add category_path(category), :lastmod => category.updated_at
-  end
-  Product.find_each do |product|
-    add category_product_path(product.category, product), :lastmod => product.updated_at
-  end
-  User.find_each do |user|
-    add user_path(user), :lastmod => user.updated_at
+if Rails.env == "enviornment"
+  SitemapGenerator::Sitemap.create do
+    Category.find_each do |category|
+      add category_path(category), :lastmod => category.updated_at
+    end
+    Product.find_each do |product|
+      add category_product_path(product.category, product), :lastmod => product.updated_at
+    end
+    User.find_each do |user|
+      add user_path(user), :lastmod => user.updated_at
+    end
   end
 end
+
 SitemapGenerator::Sitemap.ping_search_engines

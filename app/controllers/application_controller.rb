b237d5bd22450ecc87da8ca039d9307e993b2bf9
vagrant <vagrant@rails-dev-box>
2014-12-08 03:21:09 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
       @products = Product.where(holiday: true).order('rank DESC').page(params[:page]).per(10)
        @leaders =  HIGHSCORE_LB.leaders(1)
         if current_user
-      @activities = PublicActivity::Activity.where(recipient_id: current_user.id, owner_type: "User").order("created_at desc")
+      @activities = PublicActivity::Activity.where(recipient_id: current_user.id, owner_type: "User").where(read: false).order("created_at desc")
     end
   end
 
@@ -40,7 +40,7 @@ class ApplicationController < ActionController::Base
       @products = Product.where(holiday: true).order('created_at DESC').page(params[:page]).per(10)
        @leaders =  HIGHSCORE_LB.leaders(1)
         if current_user
-      @activities = PublicActivity::Activity.where(recipient_id: current_user.id, owner_type: "User").order("created_at desc")
+      @activities = PublicActivity::Activity.where(recipient_id: current_user.id, owner_type: "User").where(read: false).order("created_at desc")
     end
   end
 end

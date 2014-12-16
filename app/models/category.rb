bd3aeb85bb1cc17a2c03f9d834b952fc83d42ce4
vagrant <vagrant@rails-dev-box>
2014-12-16 05:02:16 +0000

    a

--- app/models/category.rb
+++ app/models/category.rb
@@ -23,7 +23,13 @@ class Category < ActiveRecord::Base
     hitsPerPage 40
     end
 
-  
+  def subcat_present?
+    subcategories.first
+  end
+
+  algoliasearch index_name: "Parentcats", if: :subcat_present? do
+    
+  end
 
   def should_generate_new_friendly_id?
     slug.blank? || title_changed?
